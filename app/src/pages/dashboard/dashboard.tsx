468df730e5cfdc88db022d29d8671375ae489a9e
Santas7 <andreypanyshkin03@gmail.com>
2025-06-16 13:22:46 +0400

    upd dashboard

--- app/src/pages/dashboard/dashboard.tsx
+++ app/src/pages/dashboard/dashboard.tsx
@@ -1,25 +1,80 @@
-import { Button, Title, Text } from '@mantine/core';
+import { useState } from 'react';
+import {
+  Icon2fa,
+  IconBellRinging,
+  IconDatabaseImport,
+  IconFingerprint,
+  IconKey,
+  IconLogout,
+  IconReceipt2,
+  IconSettings,
+  IconSwitchHorizontal,
+} from '@tabler/icons-react';
+import { Code, Group } from '@mantine/core';
+import { MantineLogo } from '@mantinex/mantine-logo';
+import classes from './NavbarSimple.module.css';
 import { useNavigate } from 'react-router';
 import { useAppDispatch, useAppSelector } from '../../core/store/hooks';
 import { logout } from '../../core/store/slices/auth-slice';
+
+
+const data = [
+  { link: '', label: 'Notifications', icon: IconBellRinging },
+  { link: '', label: 'Billing', icon: IconReceipt2 },
+  { link: '', label: 'Security', icon: IconFingerprint },
+  { link: '', label: 'SSH Keys', icon: IconKey },
+  { link: '', label: 'Databases', icon: IconDatabaseImport },
+  { link: '', label: 'Authentication', icon: Icon2fa },
+  { link: '', label: 'Other Settings', icon: IconSettings },
+];
 
 export const Dashboard = () => {
   const user = useAppSelector((state) => state.auth.user);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
+  const [active, setActive] = useState('Billing');
   const handleLogout = () => {
     dispatch(logout());
     navigate('/');
   };
+  const links = data.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
 
   return (
-    <div>
-      <Title order={2}>Добро пожаловать, {user?.username}!</Title>
-      <Text>Это ваш дашборд.</Text>
-      <Button onClick={handleLogout} mt="md" color="red">
-        Выйти
-      </Button>
-    </div>
+    <nav className={classes.navbar}>
+      <div className={classes.navbarMain}>
+        <Group className={classes.header} justify="space-between">
+          <MantineLogo size={28} />
+          <Code fw={700}>v3.1.2</Code>
+        </Group>
+        {links}
+      </div>
+
+      <div className={classes.footer}>
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
+          <span>Change account</span>
+        </a>
+
+        <a href="#" className={classes.link} onClick={(event) => {event.preventDefault(); handleLogout();}}>
+          <IconLogout className={classes.linkIcon} stroke={1.5} />
+          <span>Logout</span>
+        </a>
+      </div>
+    </nav>
   );
-};
+}
