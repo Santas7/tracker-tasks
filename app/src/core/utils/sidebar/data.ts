d0b519d3c4ef78611eac4e702a981200e38b5080
Santas7 <andreypanyshkin03@gmail.com>
2025-06-16 14:57:09 +0400

    upd utils (data.ts)

--- app/src/core/utils/sidebar/data.ts
+++ app/src/core/utils/sidebar/data.ts
@@ -1,8 +1,8 @@
-import { IconHome, IconTable, IconUser, IconUsersGroup } from "@tabler/icons-react";
+import { IconHome, IconUserCircle, IconListCheck, IconSitemap } from "@tabler/icons-react";
 
-export const data: { link: string; label: string; icon: any }[] = [
-  { link: '', label: 'Главная', icon: IconHome },
-  { link: '', label: 'Мой Профиль', icon: IconUser },
-  { link: '', label: 'Мои Задачи', icon: IconTable },
-  { link: '', label: 'Мои группы', icon: IconUsersGroup },
+export const data = [
+  { link: '/dashboard/main', label: 'Главная', icon: IconHome },
+  { link: '/dashboard/profile', label: 'Мой профиль', icon: IconUserCircle },
+  { link: '/dashboard/tasks', label: 'Мои задачи', icon: IconListCheck },
+  { link: '/dashboard/groups', label: 'Мои группы', icon: IconSitemap },
 ];
